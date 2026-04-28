f389786daffa31c9bfb5e40b443c2d1f73c62a2a
Brad <bradfordrobbins@yahoo.com>
2026-04-28 13:57:29 -0700

    Update SHEET_ID to a specific value

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,6 @@
-const SHEET_ID = "YOUR_SHEET_ID_HERE";
+const SHEET_ID = "1qpGEb9FEuhttf0PCVuziBQn9Qvpr33OTsitIaqEdbI0";
 const SHEET_TAB = "Indicators";
 const SHEET_URL = `https://opensheet.elk.sh/${SHEET_ID}/${SHEET_TAB}`;
 
 // thresholds + rest of dashboard.js from earlier answer…
+// https://docs.google.com/spreadsheets/d/1qpGEb9FEuhttf0PCVuziBQn9Qvpr33OTsitIaqEdbI0/edit?usp=sharing
